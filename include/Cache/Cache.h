6cd942e29023ee1dba98c07141966c3539138b41
John Bolton <odolvlobo-github@yahoo.com>
2019-02-03 16:09:31 -0800

    Uncrustify pass

--- include/Cache/Cache.h
+++ include/Cache/Cache.h
@@ -14,7 +14,7 @@
 template <typename T>
 struct CacheEntry
 {
-    T    value;         // Value
+    T value;            // Value
     bool locked;        // True if the entry is locked in the cache
     bool dirty;         // True if the entry's value has been updated.
 #if defined(FEATURE_ASYNCHRONOUS)
@@ -26,11 +26,7 @@ struct CacheEntry
 //!
 //! This generic cache is a proxy for some source of data. The cache provides a method for accessing
 //! the values in the source using a key whose form is completely determined by the user. The cache is a
-//! write-back cache and uses fully associative mapping. 
-//!
-//! The cache also provides support methods: a method to flush a value, a method to prevent an value from being
-//! flushed, and a method that pre-fetches an value from the source. Pre-fetch is asynchronous (in contrast
-//! the normal accessor, which blocks until the value is in the cache).
+//! write-back cache and uses fully associative mapping.
 //!
 //! @param  Key         Type of the key used to reference a value in the cache.
 //! @param  Value,           Type of value contained in the cache
@@ -46,17 +42,17 @@ struct CacheEntry
 //!             - readAsync
 //!             - waitForAsyncRead
 
-template< typename Key,
+template <typename Key,
           typename Value,
           typename Compare   = std::less<Key>,
-          typename Allocator = std::allocator<std::pair<Key const, CacheEntry<Value>>> >
+          typename Allocator = std::allocator<std::pair<Key const, CacheEntry<Value>>>>
 class Cache
 {
 public:
 
-    using key_type = Key;
-    using value_type = Value;
-    using compare_type = Compare;
+    using key_type       = Key;
+    using value_type     = Value;
+    using compare_type   = Compare;
     using allocator_type = Allocator;
 
     using Entry = CacheEntry<value_type>;
@@ -77,9 +73,9 @@ public:
     //! @param    key        The value to access
     //!
     //! @note    If the value does not exist, it is created.
-    value_type & operator[]( key_type const & key )
+    value_type & operator [](key_type const & key)
     {
-        Entry & entry = get( key );
+        Entry & entry = get(key);
         return entry.value;
     }
 
@@ -90,9 +86,9 @@ public:
     //! @param    key        The value to access
     //!
     //! @note    If the value does not exist, it is created.
-    value_type operator[]( key_type const & key ) const
+    value_type operator [](key_type const & key) const
     {
-        Entry & entry = get( key );
+        Entry & entry = get(key);
         return entry.value;
     }
 
@@ -114,9 +110,9 @@ public:
     //!
     //! @param    key   Which value to flush
     //!
-    void flush( key_type const & key )
+    void flush(key_type const & key)
     {
-        typename Container::iterator p = container_.find( key ); // Point to the value in the cache
+        typename Container::iterator p = container_.find(key);   // Point to the value in the cache
         if (p != container_.end())
             flush(p);
     }
@@ -141,9 +137,9 @@ public:
     //! @param  locked      The new lock setting. Default: true
     //!
     //! @note    If the value is not in the cache, it is loaded into the cache first.
-    void lock( key_type const & key, bool locked = true )
+    void lock(key_type const & key, bool locked = true)
     {
-        Entry &    entry    = get( key );
+        Entry & entry = get(key);
         entry.locked = locked;
     }
 
@@ -154,9 +150,9 @@ public:
     //! @param  key     The key of the value to mark
     //!
     //! @note    If the value is not in the cache, it is loaded into the cache first.
-    void dirty( key_type const & key )
+    void dirty(key_type const & key)
     {
-        Entry & entry = get( key );
+        Entry & entry = get(key);
         entry.dirty = true;
     }
 
@@ -167,13 +163,13 @@ public:
     //! @param  key     The key of the value to mark
     //!
     //! @warning    Invalidating a value marked as dirty results in a loss
-    void invalidate( key_type const & key )
+    void invalidate(key_type const & key)
     {
         // Instead of being marked, the value is just removed from the cache, forcing it to be loaded from the source
         // the next time it is accessed.
 
-        typename Container::iterator const    p    = container_.find( key );
-        if ( p != container_.end() )
+        typename Container::iterator const p = container_.find(key);
+        if (p != container_.end())
         {
             assert(!p->second.dirty); // Invalidating dirty data results in a loss
             container_.erase(p);
@@ -186,13 +182,11 @@ public:
     //! nothing happens.
     //!
     //! @param    key        The value to remove
-    void purge( key_type const & key )
+    void purge(key_type const & key)
     {
-        typename Container::iterator const    p    = container_.find( key );
-        if ( p != container_.end() )
-        {
-            purge( p );
-        }
+        typename Container::iterator const p = container_.find(key);
+        if (p != container_.end())
+            purge(p);
     }
 
     //! Purges all values in the cache.
@@ -201,9 +195,9 @@ public:
     //!
     void clear()
     {
-        for ( typename Container::iterator p = container_.begin(); p != container_.end(); ++p )
+        for (typename Container::iterator p = container_.begin(); p != container_.end(); ++p)
         {
-            purge( p );
+            purge(p);
         }
     }
 
@@ -214,22 +208,22 @@ public:
     //! it is loaded when it is accessed, and the accessor waits until the value is loaded before continuing.
     //!
     //! @param    key        The value to prefetch
-    void prefetch( key_type const & key )
+    void prefetch(key_type const & key)
     {
-        typename Container::iterator p = container_.find( key ); // Point to the value in the cache
+        typename Container::iterator p = container_.find(key);   // Point to the value in the cache
 
         // If the value or its stand-in is not already in the cache, then get it from the source
-        if ( p == container_.end() )
+        if (p == container_.end())
         {
-            p = insert( key, value_type(), true );
-            asyncRead( key, &p->second.value );
+            p = insert(key, value_type(), true);
+            asyncRead(key, &p->second.value);
         }
     }
 #endif // defined(FEATURE_ASYNCHRONOUS)
 
 protected:
 
-    typedef std::map< key_type, Entry, compare_type, allocator_type >        Container;
+    using Container = std::map<key_type, Entry, compare_type, allocator_type>;
 
     //! Returns the cache's underlying container.
     Container & container() { return container_; }
@@ -244,7 +238,7 @@ protected:
     //! @return     true if the cache is full
     //!
     //! @note    This function must be overridden.
-    virtual bool full( key_type const & key ) const = 0;
+    virtual bool full(key_type const & key) const = 0;
 
     //! Returns a value from the source.
     //!
@@ -254,7 +248,7 @@ protected:
     //!
     //! @note    This function must be overridden.
     //! @note    If the value doesn't exist in the source, it must be created.
-    virtual value_type read( key_type const & key ) const = 0;
+    virtual value_type read(key_type const & key) const = 0;
 
     //! Writes a value to the source.
     //!
@@ -262,7 +256,7 @@ protected:
     //! @param  value   Value to write
     //!
     //! @note    This function must be overridden
-    virtual void write( key_type const & key, value_type const & value ) const = 0;
+    virtual void write(key_type const & key, value_type const & value) const = 0;
 
     //! Returns an iterator pointing to the entry in the cache with the lowest priority.
     //!
@@ -274,7 +268,7 @@ protected:
     //!                 determine which value to overwrite.
     //!
     //! @note    This function must be overridden
-    virtual typename Container::iterator condemn( key_type const & hint ) = 0;
+    virtual typename Container::iterator condemn(key_type const & hint) = 0;
 
 #if defined(FEATURE_ASYNCHRONOUS)
     //! Retrieves a value from the source, but returns immediately, potentially before the value is in the cache.
@@ -283,7 +277,7 @@ protected:
     //! @param  pValue      Where to put the value
     //!
     //! @note    This function must be overridden
-    virtual void readAsync( key_type const & key, value_type * pValue ) const = 0;
+    virtual void readAsync(key_type const & key, value_type * pValue) const = 0;
 
     //! Waits for a value to be retrieved from the source via a previous readAsync call.
     //!
@@ -291,63 +285,59 @@ protected:
     //! @param  pValue      Where to put the value
     //!
     //! @note    This function must be overridden
-    virtual void waitForAsyncRead( key_type const & key ) const = 0;
+    virtual void waitForAsyncRead(key_type const & key) const = 0;
 #endif // defined(FEATURE_ASYNCHRONOUS)
 
 private:
 
     // Retrieves a value from the cache, loading it from the source if necessary.
-    Entry & get( key_type const & key )
+    Entry & get(key_type const & key)
     {
-        typename Container::iterator p = container_.find( key );
+        typename Container::iterator p = container_.find(key);
 
         // If the value is not in the cache, then it must be loaded now.
-        if ( p == container_.end())
-        {
+        if (p == container_.end())
             p = insert(key, read(key));
-        }
+
 #if defined(FEATURE_ASYNCHRONOUS)
         else
-        {
-            synchronize( key );
-        }
-#endif // defined(FEATURE_ASYNCHRONOUS)
+            synchronize(key);
+
+#endif  // defined(FEATURE_ASYNCHRONOUS)
 
         return p->second;
     }
 
     // Removes an entry after writing back to the source if dirty
-    void purge( typename Container::iterator const & p )
+    void purge(typename Container::iterator const & p)
     {
         // Make sure the source has been updated
-        flush( p );
+        flush(p);
 
         // Remove it
-        container_.erase( p );
+        container_.erase(p);
     }
 
     // Writes an entry back to the source if it is marked as dirty
-    void flush( typename Container::iterator const & p )
+    void flush(typename Container::iterator const & p)
     {
         key_type const & key   = p->first;
         Entry &          entry = p->second;
 
         // If the entry is dirty, write it out and mark it as not dirty.
-        if ( entry.dirty )
+        if (entry.dirty)
         {
-            write( key, entry.value );
+            write(key, entry.value);
             entry.dirty = false;
         }
     }
 
     // Inserts a new value into the cache. Returns the container iterator to the inserted entry.
-    typename Container::iterator insert( key_type const & key, value_type const & value) const
+    typename Container::iterator insert(key_type const & key, value_type const & value) const
     {
         // Make sure there is enough room in the cache for the new value
-        if ( full( key ) )
-        {
-            purge( condemn( key ) );
-        }
+        if (full(key))
+            purge(condemn(key));
 
         // Load the value into the cache
         Entry entry{ value, false, false };
@@ -357,13 +347,11 @@ private:
 
 #if defined(FEATURE_ASYNCHRONOUS)
     // Inserts a new value into the cache. Returns the container iterator to the inserted entry.
-    typename Container::iterator insert( key_type const & key, value_type const & value, bool prefetched ) const
+    typename Container::iterator insert(key_type const & key, value_type const & value, bool prefetched) const
     {
         // Make sure there is enough room in the cache for the new value
-        if ( full( key ) )
-        {
-            purge( condemn( key ) );
-        }
+        if (full(key))
+            purge(condemn(key));
 
         // Load the value into the cache
         Entry entry{ false, false, prefetched, value };
@@ -372,25 +360,25 @@ private:
     }
 
     // Synchronizes an entry that is dirty or is being prefetched
-    void synchronize( key_type const & key )
+    void synchronize(key_type const & key)
     {
         Entry & entry = get(key);
         assert(!entry.dirty || !entry.prefetched); // That would be bad
 
-        if ( entry.prefetched )
+        if (entry.prefetched)
         {
-            waitForAsyncRead( key );
+            waitForAsyncRead(key);
             entry.prefetched = false;
         }
         else if (entry.dirty)
         {
-            write( key, entry.value );
+            write(key, entry.value);
             entry.dirty = false;
         }
     }
 #endif // defined(FEATURE_ASYNCHRONOUS)
 
-    mutable Container    container_;    // The underlying container
+    mutable Container container_;       // The underlying container
 };
 
 #endif // !defined(CACHE_CACHE_H)
